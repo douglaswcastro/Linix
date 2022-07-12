975b1a861cedbc7105d7c716e1954e399a41aab7
Douglas Willian de Castro <douglaswcastro@hotmail.com>
2022-07-12 13:51:14 -0300

    adicionando comandos para instalação

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PopOsPosInstall
-Script de pos instalação no PopOS
+Script de pós instalação no PopOS
+
+Autor: Douglas Castro
+
+COMO USAR?
+
+É necessário verificar se o arquivo tem permissão de executar
+após isso entrar na pasta onde baixou e rodar o comando abaixo
+$ ./pos-os-postinstall.sh
--- posinstall.sh
+++ posinstall.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
+
+# Autor: Douglas Castro
+
+# COMO USAR?
+#   $ ./pos-os-postinstall.sh
+
 echo "Executando pos instalação"
 
+##URLS
+
+URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.20.0-1_amd64.deb?source=website"
+URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.7.2.50318-impish_amd64.deb"
+URL_SYNOLOGY_DRIVE="https://global.download.synology.com/download/Utility/SynologyDriveClient/3.0.3-12689/Ubuntu/Installer/x86_64/synology-drive-client-12689.x86_64.deb"
+
+
+##DIRETÓRIOS E ARQUIVOS
+
+DIRETORIO_DOWNLOADS="$HOME/Downloads"
+FILE="/home/$USER/.config/gtk-3.0/bookmarks"
+
+
+#CORES
+
+VERMELHO='\e[1;91m'
+VERDE='\e[1;92m'
+SEM_COR='\e[0m'
+
+
+# Atualizando repositório e fazendo atualização do sistema
+
+apt_update(){
+  echo "Atualizando repositório e fazendo atualização do sistema"
+  sudo apt update && sudo apt dist-upgrade -y
+}
+
+# Instalando Dotnet
+dotnet(){
+echo "DotNet"
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
@@ -16,4 +53,147 @@ sudo apt-get update; \
   sudo apt-get install -y aspnetcore-runtime-6.0
 
 sudo apt-get install -y dotnet-runtime-6.0
-echo "Executando com sucesso!"
+}
+
+# Internet conectando?
+testes_internet(){
+echo "Teste Internet"
+if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
+  echo -e "${VERMELHO}[ERROR] - Seu computador não tem conexão com a Internet. Verifique a rede.${SEM_COR}"
+  exit 1
+else
+  echo -e "${VERDE}[INFO] - Conexão com a Internet funcionando normalmente.${SEM_COR}"
+fi
+}
+
+## Removendo travas eventuais do apt ##
+travas_apt(){
+  sudo rm /var/lib/dpkg/lock-frontend
+  sudo rm /var/cache/apt/archives/lock
+}
+
+## Adicionando/Confirmando arquitetura de 32 bits ##
+add_archi386(){
+sudo dpkg --add-architecture i386
+}
+
+##DEB SOFTWARES TO INSTALL
+
+PROGRAMAS_PARA_INSTALAR=(
+  snapd
+  winff
+  virtualbox
+  ratbagd
+  gparted
+  timeshift
+  gufw
+  synaptic
+  solaar
+  vlc
+  code
+  gnome-sushi 
+  folder-color
+  git
+  wget
+  ubuntu-restricted-extras
+
+)
+
+## Download e instalaçao de programas externos ##
+
+install_debs(){
+
+echo -e "${VERDE}[INFO] - Baixando pacotes .deb${SEM_COR}"
+
+mkdir "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_4K_VIDEO_DOWNLOADER" -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_INSYNC"              -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_SYNOLOGY_DRIVE"      -P "$DIRETORIO_DOWNLOADS"
+
+## Instalando pacotes .deb baixados na sessão anterior ##
+echo -e "${VERDE}[INFO] - Instalando pacotes .deb baixados${SEM_COR}"
+sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+
+# Instalar programas no apt
+echo -e "${VERDE}[INFO] - Instalando pacotes apt do repositório${SEM_COR}"
+
+for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
+  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
+    sudo apt install "$nome_do_programa" -y
+  else
+    echo "[INSTALADO] - $nome_do_programa"
+  fi
+done
+
+}
+
+## Instalando pacotes Flatpak ##
+install_flatpaks(){
+
+  echo -e "${VERDE}[INFO] - Instalando pacotes flatpak${SEM_COR}"
+
+flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub org.gimp.GIMP -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub com.bitwarden.desktop -y
+flatpak install flathub org.telegram.desktop -y
+flatpak install flathub org.freedesktop.Piper -y
+flatpak install flathub org.chromium.Chromium -y
+flatpak install flathub org.gnome.Boxes -y
+flatpak install flathub org.onlyoffice.desktopeditors -y
+flatpak install flathub org.qbittorrent.qBittorrent -y
+flatpak install flathub org.flameshot.Flameshot -y
+flatpak install flathub org.electrum.electrum -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.valvesoftware.Steam -y
+flatpak install flathub org.telegram.desktop -y
+flatpak install flathub com.visualstudio.code -y
+flatpak install flathub org.videolan.VLC -y
+flatpak install flathub com.brave.Browser -y
+flatpak install flathub org.gimp.GIMP -y
+flatpak install flathub com.getpostman.Postman -y
+flatpak install flathub io.github.mimbrero.WhatsAppDesktop -y
+flatpak install flathub org.audacityteam.Audacity -y
+flatpak install flathub com.adobe.Flash-Player-Projector -y
+}
+## Instalando pacotes Snap ##
+
+install_snaps(){
+
+echo -e "${VERDE}[INFO] - Instalando pacotes snap${SEM_COR}"
+
+sudo snap install authy
+sudo snap install pycharm-professional --classic
+sudo snap install rider --classic
+sudo snap install intellij-idea-ultimate --classic
+}
+
+## Finalização, atualização e limpeza##
+
+system_clean(){
+
+apt_update -y
+flatpak update -y
+sudo apt autoclean -y
+sudo apt autoremove -y
+nautilus -q
+}
+
+travas_apt
+testes_internet
+travas_apt
+apt_update
+travas_apt
+add_archi386
+just_apt_update
+install_debs
+install_flatpaks
+install_snaps
+extra_config
+apt_update
+system_clean
+
+## finalização
+
+echo -e "${VERDE}[INFO] - Script finalizado, instalação concluída! :)${SEM_COR}"
