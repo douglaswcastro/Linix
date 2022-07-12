9329d5e7f95e1eb067a0fb59c5f0c52c99e82ce2
douglaswcastro <douglaswcastro@hotmail.com>
2022-07-12 00:55:02 -0300

    Adicionando dotnet core

--- posinstall.sh
+++ posinstall.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 echo "Executando pos instalação"
 
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-6.0
 
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-6.0
+
+sudo apt-get install -y dotnet-runtime-6.0
 echo "Executando com sucesso!"
