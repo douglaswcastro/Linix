15b5ab8ab4ad6158b9adb0a0af082465e15e7896
Douglas <douglaswcastro@hotmail.com>
2024-05-25 19:33:08 -0300

    melhorias no script

--- posinstall.sh
+++ posinstall.sh
@@ -5,204 +5,231 @@
 # COMO USAR?
 #   $ ./pos-os-postinstall.sh
 
-echo "Executando pos instalação"
+echo "Executando pós-instalação"
 
-#CORES
+# CORES
 VERMELHO='\e[1;91m'
 VERDE='\e[1;92m'
 SEM_COR='\e[0m'
 
+# Diretório de Downloads
+DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
+
+# URLs dos pacotes .deb externos
+URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.18.5-1_amd64.deb"
+URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.7.0.50216-impish_amd64.deb"
+URL_SYNOLOGY_DRIVE="https://global.download.synology.com/download/Utility/SynologyDriveClient/2.0.3-11105/Ubuntu/Installer/x86_64/synology-drive-client-11105.x86_64.deb"
+
+# PPAs e URLs das chaves
+PPA_LIBRATBAG="ppa:libratbag-piper/piper-libratbag-git"
+PPA_LUTRIS="ppa:lutris-team/lutris"
+PPA_GRAPHICS_DRIVERS="ppa:graphics-drivers/ppa"
+URL_WINE_KEY="https://dl.winehq.org/wine-builds/winehq.key"
+URL_PPA_WINE="https://dl.winehq.org/wine-builds/ubuntu/"
+
+# Teste de conexão com a Internet
+testes_internet(){
+  echo "Teste Internet"
+  if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
+    echo -e "${VERMELHO}[ERROR] - Seu computador não tem conexão com a Internet. Verifique a rede.${SEM_COR}"
+    exit 1
+  else
+    echo -e "${VERDE}[INFO] - Conexão com a Internet funcionando normalmente.${SEM_COR}"
+  fi
+}
 
 # Atualizando repositório e fazendo atualização do sistema
-
 apt_update(){
   echo "Atualizando repositório e fazendo atualização do sistema"
   sudo apt update && sudo apt dist-upgrade -y
 }
 
+# Instalando Onedriver
 onedriver(){
+  echo "Instalando Onedriver"
   echo 'deb http://download.opensuse.org/repositories/home:/jstaf/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:jstaf.list
-  curl -fsSL https://download.opensuse.org/repositories/home:jstaf/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_jstaf.gpg > /dev/null
+  curl -fsSL https://download.opensuse.org/repositories/home:/jstaf/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_jstaf.gpg > /dev/null
   sudo apt update
-  sudo apt install onedriver
+  sudo apt install -y onedriver
 }
 
+# Instalando Visual Studio Code
 code(){
-  # vs code
-	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings
-	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-	rm -f packages.microsoft.gpg
-	sudo apt update; sudo apt install -y code
+  echo "Instalando Visual Studio Code"
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm -f packages.microsoft.gpg
+  sudo apt update
+  sudo apt install -y code
 }
 
-# Internet conectando?
-testes_internet(){
-echo "Teste Internet"
-if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
-  echo -e "${VERMELHO}[ERROR] - Seu computador não tem conexão com a Internet. Verifique a rede.${SEM_COR}"
-  exit 1
-else
-  echo -e "${VERDE}[INFO] - Conexão com a Internet funcionando normalmente.${SEM_COR}"
-fi
-}
-
-## Removendo travas eventuais do apt ##
+# Removendo travas eventuais do apt
 travas_apt(){
-  sudo rm /var/lib/dpkg/lock-frontend
-  sudo rm /var/cache/apt/archives/lock
+  sudo rm -f /var/lib/dpkg/lock-frontend /var/cache/apt/archives/lock
 }
 
-## Adicionando/Confirmando arquitetura de 32 bits ##
+# Adicionando/Confirmando arquitetura de 32 bits
 add_archi386(){
-sudo dpkg --add-architecture i386
+  sudo dpkg --add-architecture i386
 }
 
-##DEB SOFTWARES TO INSTALL
+# Instalando programas do repositório APT
+install_apt_programs(){
+  echo -e "${VERDE}[INFO] - Instalando pacotes apt do repositório${SEM_COR}"
 
-PROGRAMAS_PARA_INSTALAR=(
-  winff
-  virtualbox
-  ratbagd
-  gparted
-  timeshift
-  gufw
-  synaptic
-  solaar
-  vlc
-  gnome-sushi 
-  folder-color
-  git
-  wget
-  ubuntu-restricted-extras
-  nodejs
-  npm
-  gimp
-  audacity
-  lutris
-  libvulkan1
-  libvulkan1:i386
-  libgnutls30:i386
-  libldap-2.4-2:i386
-  libgpg-error0:i386
-  libxml2:i386
-  libasound2-plugins:i386
-  libsdl2-2.0-0:i386
-  libfreetype6:i386
-  libdbus-1-3:i386
-  libsqlite3-0:i386
-  opencl-headers
-  ocl-icd-libopencl1
-  clinfo
-)
+  local programas=(
+    winff
+    virtualbox
+    ratbagd
+    gparted
+    timeshift
+    gufw
+    synaptic
+    solaar
+    vlc
+    gnome-sushi
+    folder-color
+    git
+    wget
+    ubuntu-restricted-extras
+    nodejs
+    npm
+    gimp
+    audacity
+    lutris
+    libvulkan1
+    libvulkan1:i386
+    libgnutls30:i386
+    libldap-2.4-2:i386
+    libgpg-error0:i386
+    libxml2:i386
+    libasound2-plugins:i386
+    libsdl2-2.0-0:i386
+    libfreetype6:i386
+    libdbus-1-3:i386
+    libsqlite3-0:i386
+    opencl-headers
+    ocl-icd-libopencl1
+    clinfo
+  )
 
-sudo apt-add-repository "$PPA_LIBRATBAG" -y
-sudo add-apt-repository "$PPA_LUTRIS" -y
-sudo apt-add-repository "$PPA_GRAPHICS_DRIVERS" -y
-wget -nc "$URL_WINE_KEY"
-sudo apt-key add winehq.key
-sudo apt-add-repository "deb $URL_PPA_WINE bionic main"
-sudo apt-add-repository "deb $URL_PPA_WINE focal main"
+  for programa in "${programas[@]}"; do
+    if ! dpkg -l | grep -q "$programa"; then
+      sudo apt install -y "$programa"
+    else
+      echo "[INSTALADO] - $programa"
+    fi
+  done
+}
 
-## Download e instalaçao de programas externos ##
+# Adicionando PPAs e instalando chaves
+add_ppas(){
+  echo -e "${VERDE}[INFO] - Adicionando PPAs e instalando chaves${SEM_COR}"
+  
+  sudo apt-add-repository -y "$PPA_LIBRATBAG"
+  sudo add-apt-repository -y "$PPA_LUTRIS"
+  sudo apt-add-repository -y "$PPA_GRAPHICS_DRIVERS"
+  wget -nc "$URL_WINE_KEY" -O winehq.key
+  sudo apt-key add winehq.key
+  sudo apt-add-repository "deb $URL_PPA_WINE bionic main"
+  sudo apt-add-repository "deb $URL_PPA_WINE focal main"
+}
 
+# Download e instalação de pacotes .deb externos
 install_debs(){
+  echo -e "${VERDE}[INFO] - Baixando e instalando pacotes .deb${SEM_COR}"
 
-echo -e "${VERDE}[INFO] - Baixando pacotes .deb${SEM_COR}"
+  mkdir -p "$DIRETORIO_DOWNLOADS"
+  wget -c "$URL_GOOGLE_CHROME" -P "$DIRETORIO_DOWNLOADS"
+  wget -c "$URL_4K_VIDEO_DOWNLOADER" -P "$DIRETORIO_DOWNLOADS"
+  wget -c "$URL_INSYNC" -P "$DIRETORIO_DOWNLOADS"
+  wget -c "$URL_SYNOLOGY_DRIVE" -P "$DIRETORIO_DOWNLOADS"
 
-mkdir "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_4K_VIDEO_DOWNLOADER" -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_INSYNC"              -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_SYNOLOGY_DRIVE"      -P "$DIRETORIO_DOWNLOADS"
-
-## Instalando pacotes .deb baixados na sessão anterior ##
-echo -e "${VERDE}[INFO] - Instalando pacotes .deb baixados${SEM_COR}"
-sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
-
-# Instalar programas no apt
-echo -e "${VERDE}[INFO] - Instalando pacotes apt do repositório${SEM_COR}"
-
-for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
-  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
-    sudo apt install "$nome_do_programa" -y
-  else
-    echo "[INSTALADO] - $nome_do_programa"
-  fi
-done
-
+  sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+  sudo apt-get install -f -y
 }
 
-## Instalando pacotes Flatpak ##
+# Instalando pacotes Flatpak
 install_flatpaks(){
-
   echo -e "${VERDE}[INFO] - Instalando pacotes flatpak${SEM_COR}"
 
-flatpak install flathub com.bitwarden.desktop -y
-flatpak install flathub org.telegram.desktop -y
-flatpak install flathub org.freedesktop.Piper -y
-flatpak install flathub org.chromium.Chromium -y
-flatpak install flathub org.gnome.Boxes -y
-flatpak install flathub org.onlyoffice.desktopeditors -y
-flatpak install flathub org.qbittorrent.qBittorrent -y
-flatpak install flathub org.flameshot.Flameshot -y
-flatpak install flathub org.electrum.electrum -y
-flatpak install flathub com.discordapp.Discord -y
-flatpak install flathub org.telegram.desktop -y
-flatpak install flathub com.brave.Browser -y
-flatpak install flathub com.getpostman.Postman -y
-flatpak install flathub io.github.mimbrero.WhatsAppDesktop -y
-flatpak install flathub com.adobe.Flash-Player-Projector -y
-}
-## Instalando pacotes Snap ##
+  local flatpaks=(
+    com.bitwarden.desktop
+    org.telegram.desktop
+    org.freedesktop.Piper
+    org.chromium.Chromium
+    org.gnome.Boxes
+    org.onlyoffice.desktopeditors
+    org.qbittorrent.qBittorrent
+    org.flameshot.Flameshot
+    org.electrum.electrum
+    com.discordapp.Discord
+    com.brave.Browser
+    com.getpostman.Postman
+    io.github.mimbrero.WhatsAppDesktop
+    com.adobe.Flash-Player-Projector
+  )
 
+  for flatpak in "${flatpaks[@]}"; do
+    flatpak install -y flathub "$flatpak"
+  done
+}
+
+# Instalando pacotes Snap
 install_snaps(){
-sudo apt install snapd
-echo -e "${VERDE}[INFO] - Instalando pacotes snap${SEM_COR}"
+  echo -e "${VERDE}[INFO] - Instalando pacotes snap${SEM_COR}"
 
-sudo snap install authy
-sudo snap install pycharm-professional --classic
-sudo snap install rider --classic
-sudo snap install intellij-idea-ultimate --classic
-sudo snap install spotify
-sudo snap install obs-studio
-#Dotnet 7.0
-sudo snap install dotnet-runtime-70
-#Dotnet 6.0
-sudo snap install dotnet-runtime-60
-#Dotnet SDK
-sudo snap install dotnet-sdk --classic
-#node
-sudo snap install node --classic
+  sudo apt install -y snapd
+
+  local snaps=(
+    authy
+    pycharm-professional --classic
+    rider --classic
+    intellij-idea-ultimate --classic
+    spotify
+    obs-studio
+    dotnet-runtime-80
+    dotnet-runtime-70
+    dotnet-runtime-60
+    dotnet-sdk --classic
+    node --classic
+  )
+
+  for snap in "${snaps[@]}"; do
+    sudo snap install "$snap"
+  done
 }
 
-## Finalização, atualização e limpeza##
-
+# Limpeza do sistema
 system_clean(){
-
-apt_update -y
-flatpak update -y
-sudo apt autoclean -y
-sudo apt autoremove -y
-nautilus -q
+  echo -e "${VERDE}[INFO] - Realizando limpeza do sistema${SEM_COR}"
+  
+  sudo apt update
+  sudo apt upgrade -y
+  flatpak update -y
+  sudo apt autoclean -y
+  sudo apt autoremove -y
+  nautilus -q
 }
 
+# Execução das funções
 travas_apt
 testes_internet
 travas_apt
 apt_update
 travas_apt
 add_archi386
-just_apt_update
+apt_update
 onedriver
 code
+add_ppas
 install_debs
+install_apt_programs
 install_snaps
 install_flatpaks
-extra_config
-apt_update
 system_clean
 
-## finalização
-
+# Finalização
 echo -e "${VERDE}[INFO] - Script finalizado, instalação concluída! :)${SEM_COR}"
